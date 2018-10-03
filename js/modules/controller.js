bdbd9ba5e2cfe0e21a3a69a9977d96507df45bdd
yaqeenmoh <32637905+yaqeenmoh@users.noreply.github.com>
2018-10-03 17:51:33 +0100

    controller

--- js/modules/controller.js
+++ js/modules/controller.js
@@ -7,6 +7,13 @@ app.controller('loginCtrl',function($scope,Web3jsObj){
     // login function
 
 $scope.loginBtn=function(_voter){
+    const IsVoterExist=smartInstance.checkIdAndPasswordVoter.call(nationalId,password);
+    if(IsVoterExist==true){
+        localStorage.setItem("voterId",_nationalID);
+      }
+      return IsVoterExist ;
+     
+     
     
 }
 
