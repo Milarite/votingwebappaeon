1daff4f468ed8a60dd4ef4f9608571d0445c1aaa
muath <gharablipro19933@hotmail.com>
2018-10-04 12:01:19 +0300

    login

--- js/modules/controller.js
+++ js/modules/controller.js
@@ -7,14 +7,11 @@ app.controller('loginCtrl',function($scope,Web3jsObj,$window){
     Web3jsObj.Web3Facotry(rinkebyUrl);
     const smartInstance = Web3jsObj.Web3SmartContract();
     // login function
-    Web3jsObj.web3Init(contractsInfo.main,MainAbi,public_key,private_key);
-    Web3jsObj.Web3Facotry(rinkebyUrl);
-    const smartInstance = Web3jsObj.Web3SmartContract();
+
 $scope.loginBtn=function(_voter){
-    debugger;
     const IsVoterExist=smartInstance.checkIdAndPasswordVoter.call(_voter.nationalId,_voter.password);
-    if(IsVoterExist==true){
-        localStorage.setItem("voterId",_nationalID);
+    if(IsVoterExist){
+        localStorage.setItem("voterId",_voter.nationalId);
         $window.location.href="#";
         location.reload();
       }
