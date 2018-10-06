1d15af9b99a81c665aba8787af51c730d78265cf
muath <gharablipro19933@hotmail.com>
2018-10-06 19:38:52 +0300

    changes

--- js/modules/service.js
+++ js/modules/service.js
@@ -1,4 +1,102 @@
 var app=angular.module('starter.services',[]);
+
+app.service("Helper",function(){
+
+    this.ConvertDateToTimeStamp = function (_date){
+
+        var myDate=_date;
+        myDate=myDate.split("-");
+        var newDate=myDate[1]+"/"+myDate[0]+"/"+myDate[2];
+        return new Date(newDate).getTime()
+
+    }
+
+    this.ConvertTimeStampToDate = function (_timeStamp){
+        let d =  new Date(_timeStamp * 1000);
+        return moment(d).format("MM/DD/YYYY hh:MM:ss");
+    }
+ this.ConvertTimeStampToTime = function (_date){
+     let d = new Date(_date);
+        return moment(d).format("hh:MM");
+    }
+    
+    this.SplitTime = function (Period){
+ const SplitDash =Period.split("-");
+const Split1=SplitDash[1].split(":");
+let TimeINt =parseInt(Split1[0]);
+return TimeINt;
+    }
+    this.SplitTimeV2 = function(Period){
+     
+    const Split1=Period.split(":");
+    let TimeINt =parseInt(Split1[0]);
+    return TimeINt;
+        
+    }
+     this.TimeFormat = function (date){
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        minutes = minutes < 10 ? '0'+minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+     }
+
+     this.TimeConvertTo24 = function (_time){
+        var time = _time;
+        var hours = Number(time.match(/^(\d+)/)[1]);
+        var minutes = Number(time.match(/:(\d+)/)[1]);
+        var AMPM = time.match(/\s(.*)$/)[1];
+        if (AMPM == "PM" && hours < 12) hours = hours + 12;
+        if (AMPM == "AM" && hours == 12) hours = hours - 12;
+        var sHours = hours.toString();
+        var sMinutes = minutes.toString();
+        if (hours < 10) sHours = "0" + sHours;
+        if (minutes < 10) sMinutes = "0" + sMinutes;
+        return sHours+":"+sMinutes;
+     }
+    this.ConvertTimeStampTodDateFormat = function (_date){
+
+        const _DateSplited = _date.split("/");
+        const date = _DateSplited[1] +"/"+ _DateSplited[0]+"/"+_DateSplited[2];
+        let d = new Date(date);
+        
+           return moment(d).format("MM/DD/YYYY");
+       }
+       this.ConvertTimeStampTodDateFormatV2 = function (_date){
+
+       
+        let d = new Date(_date);
+        
+           return moment(d).format("MM/DD/YYYY");
+       }
+    this.formatDate = function (date){
+        var d = new Date(date);
+        
+        month = '' + (d.getMonth() + 1);
+        day = '' + d.getDate();
+
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [day,month,year].join('-');
+    }
+
+    this.isFirstDateGreaterThanSecond = function(_date1,_date2){
+  
+        let firstDate = _date1.split("/");
+        let secondDate = _date2.split("/");
+        let Date1 = new Date(_date1[1],_date1[0],_date1[2]);
+        let Date2 = new Date(_date2[1],_date2[0],_date2[2]);
+
+        return Date1 > Date2;
+
+    }
+});
 app.service("Web3jsObj",function(){
 
 
