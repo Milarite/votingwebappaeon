10fa92b1546dd05df8ded6e2915ca78f1391c1d2
muath <gharablipro19933@hotmail.com>
2018-10-08 16:13:59 +0300

    logout

--- js/modules/app.js
+++ js/modules/app.js
@@ -1,4 +1,9 @@
 angular.module('starter', [ 'starter.controllers','starter.services']).run(function($rootScope)
 {
-
+   const isLogin =  localStorage.getItem("voterId")
+        // end of check if loged in )
+        if(!localStorage.getItem("voterId"))
+$rootScope.notLogin = true;
+else
+$rootScope.notLogin = false;
 });
